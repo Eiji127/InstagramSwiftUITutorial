980e3e95fea1e83ef15730ca039b053e65645bbe
Eiji127 <64912886+Eiji127@users.noreply.github.com>
2022-06-26 16:50:38 +0900

    [Upload]: setup project

---
+++ InstagramSwiftUITutorial/Utils/InstagramSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUITutorialApp.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by 白数叡司 on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
